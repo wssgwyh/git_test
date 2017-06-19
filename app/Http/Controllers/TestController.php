de57324b6fd3b3afbaee933fbe3d3dc78641e8f9
wangyuhao <wssgwyh@sina.com>
2017-06-19 19:51:39 +0800

    first commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangyuhao
+ * Date: 2017/6/19
+ * Time: 16:13
+ */
+
+namespace App\Http\Controllers;
+
+use App\Http\Model\Test;
+
+class TestController extends Controller
+{
+    public function test()
+    {
+
+
+    }
+}
---
+++ app/Http/Model/Test.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Model;
+
+use Illuminate\Database\Eloquent\Model;
+/**
+ * Created by PhpStorm.
+ * User: wangyuhao
+ * Date: 2017/6/19
+ * Time: 16:13
+ */
+class Test extends Model
+{
+    public $table='lh_data';
+//    public $timestamps = false;
+}
+
